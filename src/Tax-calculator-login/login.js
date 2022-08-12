ff2f404bfa8b6ee58f22198b3e8e6aba7f5d2849
Arigbo <arigbojesse75@gmail.com>
2022-08-11 23:43:33 -1200

    New

--- src/Tax-calculator-login/login.js
+++ src/Tax-calculator-login/login.js
@@ -2,18 +2,14 @@ import React from "react";
 function Login() {
     return (
         <div>
-            <section>
+            <form>
                 <div className="first-name">
                     <p><span>TAX</span>Calculator</p>
                 </div>
-                <section>
-                    <div>
-                        <div>
-
-                        </div>
-                    </div>
-                </section>
-            </section>
+                <div>
+                    
+                </div>
+            </form>
         </div>
     )
 }
